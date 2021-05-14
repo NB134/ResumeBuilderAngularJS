f0f6f4d668fdfa39ddc14ddd9f877aaeace9f7c9
NB134 <badyalnischey@gmail.com>
2021-05-14 17:36:34 +0530

    Added form validation, modified controllers to keep retain the data between views

--- step2/step2.controller.js
+++ step2/step2.controller.js
@@ -1,4 +1,4 @@
-angular.module("ResumeBuilder").controller("step2", function($scope,$rootScope,$location,$timeout)
+angular.module("ResumeBuilder").controller("step2", function($scope,$rootScope,$location,$timeout,$mdDialog)
 {
     $scope.User={};
     $scope.$on("step1",function(event, args)
@@ -8,12 +8,44 @@ angular.module("ResumeBuilder").controller("step2", function($scope,$rootScope,$
         console.log("updated user",$scope.User);
     });
 
-    $scope.submitData=function(){
+    $scope.$on("step3",function(event, args)
+    {
+        console.log("hello");
+        $scope.User={...args};
+        console.log("updated user",$scope.User);
+    });
+    $scope.showAlert = function (ev) {
+        $mdDialog.show(
+          $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title('Validation Error')
+            .textContent('Please fill in the details before submitting.')
+            .ariaLabel('Alert Dialog Demo')
+            .ok('Got it!')
+            .targetEvent(ev)
+        );
+      };
+    $scope.submitData=function(flag){
         console.log($scope.User);
         $timeout(function(){
             // $scope.$broadcast('someEvent', 'bidule');
             $rootScope.$broadcast("step2", $scope.User);
          },100);
-          $location.path( "/step3" );
+         if(flag==0)
+         {
+            $location.path( "/" );    
+         }
+         else
+         {
+             if(!$scope.userForm.$valid)
+             {
+                $scope.showAlert();
+                 return undefined;
+             }
+            $location.path( "/step3" );
+         }
+         
+         
     }
 });
--- step3/step3.controller.js
+++ step3/step3.controller.js
@@ -1,18 +1,26 @@
-angular.module("ResumeBuilder").controller("step3",function($scope,$rootScope,$timeout,$location){
-    $scope.User={};
-    $scope.$on("step2",function(event, args)
-    {
-        console.log("hello");
-        $scope.User={...args};
-        console.log("updated user",$scope.User);
+angular
+  .module("ResumeBuilder")
+  .controller("step3", function ($scope, $rootScope, $timeout, $location) {
+    $scope.User = {};
+    $scope.$on("step2", function (event, args) {
+      console.log("hello");
+      $scope.User = { ...args };
+      console.log("updated user", $scope.User);
     });
 
-    $scope.submitData=function(){
-        console.log($scope.User);
-        $timeout(function(){
-            // $scope.$broadcast('someEvent', 'bidule');
-            $rootScope.$broadcast("step3", $scope.User);
-         },100);
-          $location.path( "/resume" );
-    }
-});
+
+
+    $scope.submitData = function (flag) {
+      console.log($scope.User);
+      $timeout(function () {
+        // $scope.$broadcast('someEvent', 'bidule');
+        $rootScope.$broadcast("step3", $scope.User);
+      }, 100);
+      if (flag == 0) {
+        $location.path("/step2");
+      } else {
+        
+        $location.path("/resume");
+      }
+    };
+  });
